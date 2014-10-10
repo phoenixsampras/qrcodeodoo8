e29e3ebb0498c21123fbeb15884f8057531d4f71
Vacha Trivedi <v.trivedi.serpentcs@gmail.com>
2014-10-10 16:50:19 +0530

    [IMP]account_invoice_report:Did Changes in version number,Removed version attribute from view.

--- account_invoice_report/__openerp__.py
+++ account_invoice_report/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name' : 'Invoice QR Code Report',
-    'version' : '1.0',
+    'version' : '0.01',
     'author' : 'Serpent Consulting Services Pvt. Ltd.',
     'category' : 'Accounting',
     'description' : """
